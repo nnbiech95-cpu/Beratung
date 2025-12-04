d58a62ef046ecf85d7f4a3bc12bf06e534f476d5
User <user@example.com>
2025-12-04 12:03:22 +0100

    Change base path to relative

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Beratung/', // For GitHub Pages
+  base: './', // Relative base path for flexibility
 })
